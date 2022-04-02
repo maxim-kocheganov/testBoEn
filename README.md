3b9257233f3ebfd72dcfef7a9b15557cf9c2d302
Max <maxim.k.elasnce@gmail.com>
2022-04-02 09:25:49 +0300

    Stuff

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# testBoEn
+# test for program tactics
+
+## installation
+1) python -m venv .venv
+2).\.venv\Sripts\activate
+3) pip install -r .\requirements.txt
--- exeldb/exeldb/urls.py
+++ exeldb/exeldb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from table import views as tw
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', tw.index)
 ]
